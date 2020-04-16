dc0eadd6333d27542b634ae883757ee66b962dfb
princebansal <prince.bansal@traveloka.com>
2020-04-16 15:19:30 +0530

    Changes api host

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  host: "http://localhost:8080",
+  host: "http://trappd-server-elb-1119120693.ap-south-1.elb.amazonaws.com",
 };
 
 export function handleErrors(response) {
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import rootReducer from "./reducers";
 export default createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunkMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunkMiddleware)
+    /*,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
   )
 );
